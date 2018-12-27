8ddb46ba6ac37452b12669e16bd893dbc06a77e1
k4gi <k4gi@pm.me>
2018-12-27 12:46:50 +1100

    diagonal buttons

--- examplez_button.java
+++ examplez_button.java
@@ -15,10 +15,13 @@ public class QuitButtonEx extends JFrame {
     private void initUI() {
 
         var quitButton = new JButton("Quit");
+		//add more button
+		var blankButton = new JButton("Hi");
+		var eggButton = new JButton("egg");
 
         quitButton.addActionListener((event) -> System.exit(0));
 
-        createLayout(quitButton);
+        createLayout(quitButton, blankButton, eggButton);
 
         setTitle("Quit button");
         setSize(300, 200);
@@ -36,10 +39,16 @@ public class QuitButtonEx extends JFrame {
 
         gl.setHorizontalGroup(gl.createSequentialGroup()
                 .addComponent(arg[0])
+				//add buttons
+				.addComponent(arg[1])
+				.addComponent(arg[2])
         );
 
         gl.setVerticalGroup(gl.createSequentialGroup()
                 .addComponent(arg[0])
+				//add buttons
+				.addComponent(arg[1])
+				.addComponent(arg[2])
         );
     }
 
