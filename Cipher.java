71bca6312b9d6128557e24d9bc2eeb390910b14f
k4gi <k4gi@pm.me>
2019-01-01 16:28:52 +1100

    sub cipher but it doesn't do anything yet

---
+++ Cipher.java
@@ -0,0 +1,65 @@
+import javax.swing.GroupLayout;
+import javax.swing.JFrame;
+import javax.swing.JComponent;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Cipher extends JFrame {
+	public Cipher() {
+		initUI();
+	}
+	
+	private String encodeText(String in) {
+		return in;
+	}
+	
+	private void initUI() {
+		var in_field = new JTextArea("Type here",5,50);
+		in_field.setLineWrap(true);
+		var out_field = new JTextArea("Read here",5,50);
+		out_field.setLineWrap(true);
+		
+		var press_this = new JButton("Press to encode text!");
+		press_this.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				out_field.setText( encodeText(in_field.getText()) );
+			}
+		});
+		
+		
+		createLayout(in_field, press_this, out_field);
+		setTitle("It's cipher tiem");
+		setLocationRelativeTo(null); //centre window
+		setDefaultCloseOperation(EXIT_ON_CLOSE); //make X button work
+	}
+	
+	private void createLayout(JComponent... arg) {
+		var pane = getContentPane();
+        var gl = new GroupLayout(pane);
+        pane.setLayout(gl);
+		
+		gl.setHorizontalGroup(gl.createParallelGroup()
+			.addComponent(arg[0])
+			.addComponent(arg[1])
+			.addComponent(arg[2])
+		);
+		gl.setVerticalGroup(gl.createSequentialGroup()
+			.addComponent(arg[0])
+			.addComponent(arg[1])
+			.addComponent(arg[2])
+		);
+		
+		pack(); //auto-size
+		
+	}
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(() -> {
+            var ex = new Cipher();
+            ex.setVisible(true);
+        });
+	}
+}
