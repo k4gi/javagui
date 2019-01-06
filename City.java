2f0a82808f52105c74c7f23a020169109e7479c4
k4gi <k4gi@pm.me>
2019-01-06 18:40:53 +1100

    check check check

--- City.java
+++ City.java
@@ -1,6 +1,12 @@
 import javax.swing.JFrame;
 import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.GroupLayout;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /*
 	This project is a city-building game of some sort.
@@ -32,6 +38,50 @@ import java.awt.EventQueue;
 */
 public class City extends JFrame {
 	public City() {
+		int total_workers = 10; //ten? why not
+		
+		var fisher_label = new JLabel("Fishers:");
+		var cutter_label = new JLabel("Cutters:");
+		
+		var fisher_read = new JTextField();
+		var cutter_read = new JTextField();
+		
+		var check_workers = new JButton("Check Workers");
+		check_workers.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if ( check_workers_here( total_workers, fisher_read.getText(), cutter_read.getText() ) ) {
+					fisher_read.setText("Hey it worked");
+				} else cutter_read.setText("coffee broke");
+			}
+		});
+		
+		createLayout(fisher_label, fisher_read, cutter_label, cutter_read, check_workers);
+		
+		setTitle("City Builder");
+		setLocationRelativeTo(null); //centre window
+		setDefaultCloseOperation(EXIT_ON_CLOSE); //make X button work
+	}
+	
+	private boolean check_workers_here( int total, String fisher, String cutter ) {
+		/*
+			ive gotta put checks here (lol) for like...
+				if the player gave the right kind of input (numbers!)
+				then if the numbers together are greater than total
+				wait was there more to it? it felt more complicated in my head
+		*/
+		int fisher_p;
+		int cutter_p;
+		try {
+			fisher_p = Integer.parseInt(fisher);
+			cutter_p = Integer.parseInt(cutter);
+		} catch(NumberFormatException e) {
+			return false;
+		}
+		if( fisher_p + cutter_p > total ) {
+			return false;
+		} else {
+			return true;
+		}
 	}
 	
 	private void createLayout(JComponent... arg) {
@@ -39,11 +89,32 @@ public class City extends JFrame {
         var gl = new GroupLayout(pane);
         pane.setLayout(gl);
 		
-		gl.setHorizontalGroup(gl.createParallelGroup()
+		gl.setHorizontalGroup(gl.createSequentialGroup()
+			.addGroup(gl.createParallelGroup()
+				.addGroup(gl.createSequentialGroup()
+					.addComponent(arg[0])
+					.addComponent(arg[1])
+				)
+				.addGroup(gl.createSequentialGroup()
+					.addComponent(arg[2])
+					.addComponent(arg[3])
+				)
+				.addComponent(arg[4])
+			)
 			
 		);
-		gl.setVerticalGroup(gl.createSequentialGroup()
-			
+		gl.setVerticalGroup(gl.createParallelGroup()
+			.addGroup(gl.createSequentialGroup()
+				.addGroup(gl.createParallelGroup()
+					.addComponent(arg[0])
+					.addComponent(arg[1])
+				)
+				.addGroup(gl.createParallelGroup()
+					.addComponent(arg[2])
+					.addComponent(arg[3])
+				)
+				.addComponent(arg[4])
+			)
 		);
 		
 		pack(); //auto-size
@@ -52,7 +123,7 @@ public class City extends JFrame {
 	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(() -> {
-            var ex = new Cipher();
+            var ex = new City();
             ex.setVisible(true);
         });
 	}
