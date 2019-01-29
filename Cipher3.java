f2e356f64cc6dd85ec4c441c5934320e71aefe59
Jason Lionel Wright <jason_lionel@hotmail.co.uk>
2019-01-29 12:26:12 +1100

    notes

--- Cipher3.java
+++ Cipher3.java
@@ -164,47 +164,58 @@ public class Cipher extends JFrame {
 			code_letter = plugboard.charAt( keyboard.indexOf(code_letter) );
 			debug("Plugboard... " + code_letter);
 			
-			if(false) { //well if it doesn't work why run it
-				//just having some trouble here working out the code...
-				//i'm sure it'll come together soon but its confusing...
-				//found an Enigma project made with an arduino. Pretty cool.
-				// https://www.instructables.com/id/Make-your-own-Enigma-Replica/
-				
-				//Step 4: Wheel III
-				
-				code_letter = working_wheel_iii[1].charAt( keyboard.indexOf(code_letter) );
-				debug("Wheel III... " + code_letter);
-				
-				//Step 5: Wheel II
-				
-				code_letter = working_wheel_ii[1].charAt( working_wheel_iii[0].indexOf(code_letter) );
-				debug("Wheel II...  " + code_letter);
-				
-				//Step 6: Wheel I
-				
-				code_letter = working_wheel_i[1].charAt( working_wheel_ii[0].indexOf(code_letter) );
-				debug("Wheel I...   " + code_letter);
-				
-				//Step 7: Reflector ... Let's just use reflector B for now
-				
-				code_letter = reflector_b.charAt( working_wheel_i[0].indexOf(code_letter) );
-				debug("Reflector... " + code_letter);
-				
-				//Step 8: Wheel I
-				
-				code_letter = working_wheel_i[1].charAt( working_wheel_ii[0].indexOf(code_letter) );
-				debug("Wheel I...   " + code_letter);
-				
-				//Step 9: Wheel II
-				
-				code_letter = working_wheel_ii[1].charAt( working_wheel_iii[0].indexOf(code_letter) );
-				debug("Wheel II...  " + code_letter);
-				
-				//Step 10: Wheel III
-				
-				code_letter = working_wheel_iii[1].charAt( keyboard.indexOf(code_letter) );
-				debug("Wheel III... " + code_letter);
-			}
+			//just having some trouble here working out the code...
+			//i'm sure it'll come together soon but its confusing...
+			//found an Enigma project made with an arduino. Pretty cool.
+			// https://www.instructables.com/id/Make-your-own-Enigma-Replica/
+			//it's got the code there too so I can check that out when I'm done
+			
+			
+			/*
+				I think the problem I'm having is with the directionality of the wheels.
+				Not sure though. I've just gotta work it out, make sure all the wheels are connected the right way.
+				Each wheel has contacts that align with whatever position they're each at. It's pretty neat.
+				I don't even know what my two sets of characters on each wheel really mean. It's a bit abstract.
+				A becomes E? E becomes L? Z becomes J? Or is it the other way around? Does it matter? I guess it does.
+				The site I got the characters from has some diagrams that would probably be helpful.
+				http://users.telenet.be/d.rijmenants/en/enigmatech.htm
+			*/
+			
+			
+			//Step 4: Wheel III
+			
+			code_letter = working_wheel_iii[1].charAt( keyboard.indexOf(code_letter) );
+			debug("Wheel III... " + code_letter);
+			
+			//Step 5: Wheel II
+			
+			code_letter = working_wheel_ii[1].charAt( working_wheel_iii[0].indexOf(code_letter) );
+			debug("Wheel II...  " + code_letter);
+			
+			//Step 6: Wheel I
+			
+			code_letter = working_wheel_i[1].charAt( working_wheel_ii[0].indexOf(code_letter) );
+			debug("Wheel I...   " + code_letter);
+			
+			//Step 7: Reflector ... Let's just use reflector B for now
+			
+			code_letter = reflector_b.charAt( working_wheel_i[0].indexOf(code_letter) );
+			debug("Reflector... " + code_letter);
+			
+			//Step 8: Wheel I
+			
+			code_letter = working_wheel_i[1].charAt( working_wheel_ii[0].indexOf(code_letter) );
+			debug("Wheel I...   " + code_letter);
+			
+			//Step 9: Wheel II
+			
+			code_letter = working_wheel_ii[1].charAt( working_wheel_iii[0].indexOf(code_letter) );
+			debug("Wheel II...  " + code_letter);
+			
+			//Step 10: Wheel III
+			
+			code_letter = working_wheel_iii[1].charAt( keyboard.indexOf(code_letter) );
+			debug("Wheel III... " + code_letter);
 			
 			//Step 11: Plugboard
 			
